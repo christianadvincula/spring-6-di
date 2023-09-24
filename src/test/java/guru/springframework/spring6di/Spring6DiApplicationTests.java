260f98f51393f8bf5868f5ff3fdc36728ad392db
cadvincula <CHRISTIANADVINCULA@GMAIL.COM>
2023-09-24 14:14:01 +0800

    02-spring-context-test

--- src/test/java/guru/springframework/spring6di/Spring6DiApplicationTests.java
+++ src/test/java/guru/springframework/spring6di/Spring6DiApplicationTests.java
@@ -1,10 +1,30 @@
 package guru.springframework.spring6di;
 
+import guru.springframework.spring6di.controllers.MyController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class Spring6DiApplicationTests {
+
+	@Autowired
+	ApplicationContext applicationContext;
+
+	@Autowired
+	MyController myController;
+
+	@Test
+	void testAutoWireOfMyController() {
+		System.out.println(myController.sayHello());
+	}
+
+	@Test
+	void testGetControllerFromAppCtx() {
+		MyController controller = this.applicationContext.getBean(MyController.class);
+		System.out.println(controller.sayHello());
+	}
 
 	@Test
 	void contextLoads() {
