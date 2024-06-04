9981df349417abb52cd92a524d78dcf5b6f031ea
christianadvincula <christianadvincula@gmail.com>
2024-06-05 06:08:39 +0800

    Added test for calling controller via ApplicationContext or Autowired. Included also the assertEquals test for both methods

--- src/test/java/guru/springframework/spring6di/Spring6DiApplicationTests.java
+++ src/test/java/guru/springframework/spring6di/Spring6DiApplicationTests.java
@@ -1,10 +1,34 @@
 package guru.springframework.spring6di;
 
+import guru.springframework.spring6di.controllers.MyController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class Spring6DiApplicationTests {
+
+	@Autowired
+	ApplicationContext applicationContext;
+
+	@Autowired
+	MyController myController;
+
+	@Test
+	void testAutowiredMyController() {
+		System.out.println("testAutowiredMyController: "+myController.sayHello());
+		assertEquals(myController.sayHello(), "Hello Everyone!!!!", "sayHello returned String is Hello Everyone!!!!");
+	}
+
+	@Test
+	void testGetControllerFromApplicationContext() {
+		MyController controller = applicationContext.getBean(MyController.class);
+		System.out.println("testGetControllerFromApplicationContext: "+controller.sayHello());
+		assertEquals(myController.sayHello(), "Hello Everyone!!!!", "sayHello returned String is Hello Everyone!!!!");
+	}
 
 	@Test
 	void contextLoads() {
