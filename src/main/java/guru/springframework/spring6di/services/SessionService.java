1eb7eae6b3e7889600c4f7e9acdabb776b85d797
christianadvincula <christianadvincula@gmail.com>
2024-07-06 11:58:56 +0800

    use @SessionScope

--- src/main/java/guru/springframework/spring6di/services/SessionService.java
+++ src/main/java/guru/springframework/spring6di/services/SessionService.java
@@ -2,9 +2,10 @@ package guru.springframework.spring6di.services;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.context.annotation.SessionScope;
 
 @Component
-@SessionAttributes
+@SessionScope
 public class SessionService {
     public void performService() {
         System.out.println("SessionService instance: " + this);
