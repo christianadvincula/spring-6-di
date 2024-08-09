79382e0e2af11ab61dfcfaa35d0c638ef8269eec
christianadvincula <christianadvincula@gmail.com>
2024-08-09 11:21:01 +0800

    modified unit test to get 100% test coverage of Spring6DiApplication

--- src/test/java/guru/springframework/spring6di/Spring6DiApplicationTests.java
+++ src/test/java/guru/springframework/spring6di/Spring6DiApplicationTests.java
@@ -1,76 +1,45 @@
-package guru.springframework.spring6di;
+	package guru.springframework.spring6di;
 
-import guru.springframework.spring6di.controllers.MyController;
-import org.junit.jupiter.api.Test;
-<<<<<<< Updated upstream
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
+	import guru.springframework.spring6di.controllers.MyController;
+	import org.junit.jupiter.api.Test;
+	import org.junit.jupiter.api.extension.ExtendWith;
+	import org.springframework.beans.factory.annotation.Autowired;
+	import org.springframework.boot.test.context.SpringBootTest;
+	import org.springframework.boot.test.mock.mockito.MockBean;
+	import org.springframework.boot.test.system.CapturedOutput;
+	import org.springframework.boot.test.system.OutputCaptureExtension;
+	import org.springframework.context.ApplicationContext;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-=======
-import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.ApplicationContext;
-import org.springframework.boot.test.system.CapturedOutput;
-import org.springframework.boot.test.system.OutputCaptureExtension;
-import org.junit.jupiter.api.extension.ExtendWith;
+	import static org.junit.jupiter.api.Assertions.assertNotNull;
+	import static org.mockito.Mockito.when;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.when;
->>>>>>> Stashed changes
+	@SpringBootTest
+	@ExtendWith(OutputCaptureExtension.class)
+	class Spring6DiApplicationTests {
+		@MockBean
+		private MyController myController;
 
-@SpringBootTest
-@ExtendWith(OutputCaptureExtension.class)
-class Spring6DiApplicationTests {
+		@Autowired
+		private ApplicationContext context;
 
-<<<<<<< Updated upstream
-	@Autowired
-	ApplicationContext applicationContext;
+		@Test
+		void contextLoads() {
+			assertNotNull(context);
+		}
 
-	@Autowired
-	MyController myController;
+		@Test
+		void testMain(CapturedOutput output) {
+			// Mocking the sayHello method
+			when(myController.sayHello()).thenReturn("Hello Everyone!!!!");
 
-	@Test
-	void testAutowiredMyController() {
-		System.out.println("testAutowiredMyController: "+myController.sayHello());
-		assertEquals(myController.sayHello(), "Hello Everyone!!!!", "sayHello returned String is Hello Everyone!!!!");
+			// Running the main method
+			String[] args = {};
+			Spring6DiApplication.main(args);
+
+			// Verifying the output contains expected lines
+			assertNotNull(output);
+			String out = output.getOut();
+			assert(out.contains("In Main Method"));
+			assert(out.contains("Hello Everyone!!!!"));
+		}
 	}
-
-	@Test
-	void testGetControllerFromApplicationContext() {
-		MyController controller = applicationContext.getBean(MyController.class);
-		System.out.println("testGetControllerFromApplicationContext: "+controller.sayHello());
-		assertEquals(myController.sayHello(), "Hello Everyone!!!!", "sayHello returned String is Hello Everyone!!!!");
-	}
-=======
-	@MockBean
-	private MyController myController;
-
-	@Autowired
-	private ApplicationContext context;
->>>>>>> Stashed changes
-
-	@Test
-	void contextLoads() {
-		assertNotNull(context);
-	}
-
-	@Test
-	void testMain(CapturedOutput output) {
-		// Mocking the sayHello method
-		when(myController.sayHello()).thenReturn("Hello Everyone!!!!");
-
-		// Running the main method
-		String[] args = {};
-		Spring6DiApplication.main(args);
-
-		// Verifying the output contains expected lines
-		assertNotNull(output);
-		String out = output.getOut();
-		assert(out.contains("In Main Method"));
-		assert(out.contains("Hello Everyone!!!!"));
-	}
-}
