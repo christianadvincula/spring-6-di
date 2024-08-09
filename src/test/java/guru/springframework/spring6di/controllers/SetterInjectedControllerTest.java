012a8432ec6f7ead4f0eb543c86820e1570d5fea
christianadvincula <christianadvincula@gmail.com>
2024-08-09 16:37:31 +0800

    dependency injection without spring

---
+++ src/test/java/guru/springframework/spring6di/controllers/SetterInjectedControllerTest.java
@@ -0,0 +1,22 @@
+package guru.springframework.spring6di.controllers;
+
+import guru.springframework.spring6di.services.GreetingServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SetterInjectedControllerTest {
+    private SetterInjectedController controller;
+
+    @BeforeEach
+    void setUp() {
+        controller = new SetterInjectedController();
+        controller.setGreetingService(new GreetingServiceImpl());
+    }
+
+    @Test
+    void sayHello() {
+        controller.sayHello();
+    }
+}
