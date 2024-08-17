10867fb0ff9b3b6a84f11adb7d9ad6751b75bcf1
christianadvincula <christianadvincula@gmail.com>
2024-08-17 20:27:24 +0800

    correct the MyController and MyControllerTest

--- src/main/java/guru/springframework/spring6di/controllers/MyController.java
+++ src/main/java/guru/springframework/spring6di/controllers/MyController.java
@@ -1,7 +1,6 @@
 package guru.springframework.spring6di.controllers;
 
 import guru.springframework.spring6di.services.GreetingService;
-import guru.springframework.spring6di.services.GreetingServiceImpl;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -9,8 +8,8 @@ public class MyController {
 
     private final GreetingService greetingService;
 
-    public MyController() {
-        this.greetingService = new GreetingServiceImpl();
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
     }
 
     public String sayHello() {
